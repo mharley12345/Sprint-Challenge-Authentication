c0019a22abf1757868398d76762605c565ec8816
Mike Harley <mharley12345@gmail.com>
2019-10-18 08:51:56 -0700

    adds register

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const bycrypt = require('bcryptjs')
+const Users = require('../users/users-model')
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bycrypt.hashSync(user.password,10);
+  user.password = hash;
+
+  Users.add(user)
+  .then(saved =>{
+    res.status(201).json({message:`${saved.username} successfully added`})
+  })
+  .catch(error =>{
+    res.status(500).json(error)
+  })
 });
 
 router.post('/login', (req, res) => {
